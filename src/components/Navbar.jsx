a31fb25cbbe96aa7da66bf6e68834d50cdd44026
Sudhanshu Tiwari <sudhanshu.98t@gmail.com>
2024-04-25 21:31:55 +0530

    Added 3D scenes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
         <nav className="flex text-lg gap-7 font-medium">
             <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
                 About
-            <NavLink/>
+            </NavLink>
             <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
                 Projects
-            <NavLink/>
+            </NavLink>
         </nav>
     </header>
   )
