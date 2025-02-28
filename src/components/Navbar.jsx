9f2849d4399dd4222a786580ca56f10abe08fa61
Sudhanshu Tiwari <sudhanshu.98t@gmail.com>
2025-03-01 00:32:33 +0530

    Update link for resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
                 <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
                     Projects
                 </NavLink>
-                <NavLink to="https://drive.google.com/file/d/1qjRP0rTFUoLKbB52-HdR-0u_eamQ_dSv/view?usp=sharing" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                <NavLink to="https://drive.google.com/file/d/1ZyAUdyHoPur9ar-mA2hH3hUo-NpxR1Yf/view?usp=sharing" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
                     Resume
                 </NavLink>
                 <NavLink to="https://sudhanshut.blogspot.com/" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
